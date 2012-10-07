c0fd8348e3c627b5fb7d5b891fbe01ab6c8d8637
Adrian Mowat <adrian.mowat@gmail.com>
2012-10-07 10:09:57 +0100

    Created shell and git configs

---
+++ git.sh
@@ -0,0 +1,5 @@
+alias gc='git commit -m'
+alias ga='git add'
+alias gs='git status'
+alias glo='git log --oneline'
+alias gl="git log --graph --pretty=\"format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset\""
---
+++ shell.sh
@@ -0,0 +1,9 @@
+alias ll='ls -ltr | tail'
+alias l='ls -1tr | tail'
+alias fndr='open /System/Library/CoreServices/Finder.app "$PWD"'
+
+alias sub='sublime'
+
+PS1='\[\033[0;33m\]$(rvm current)\[\033[0m\]:$(basename $PWD) \[\033[0m\]$ '
+CLICOLOR=1; export CLICOLOR
+LSCOLORS=GxFxCxDxBxegedabagaced; export LSCOLORS
