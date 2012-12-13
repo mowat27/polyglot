f108b2dadbacfc0f5542a9096d106d5a27183dc6
Adrian Mowat <adrian.mowat@arnoldclark.co.uk>
2012-12-13 09:37:22 +0000

    Removed stuff we don't need

--- cucumber.sh
+++
@@ -1,3 +0,0 @@
-CUCUMBER_COLORS=pending_param=magenta:failed_param=magenta:passed_param=magenta:skipped_param=magenta
-export CUCUMBER_COLORS
-alias cuke='jruby -S cucumber'
--- emacs.sh
+++
@@ -1,1 +0,0 @@
-alias e='open -a emacs'
